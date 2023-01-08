f6d61d7c55905849278ce034b5a295e0da8fec0a
SdxSdySdz <timurok97@gmail.com>
2023-01-08 18:46:07 +0300

    Delete extra

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.contrib.fsm_storage.redis import RedisStorage2
-
-from core.handlers import EchoHandler, CommandStartHandler
-from core.bot import TelegramBot, register_all_middlewares, register_all_filters, register_all_handlers
+from core.bot import TelegramBot
 from tgbot.config import load_config
-from tgbot.filters.admin import AdminFilter
-from tgbot.handlers.admin import register_admin
-from tgbot.handlers.echo import register_echo
-from tgbot.handlers.user import register_user
-from tgbot.middlewares.environment import EnvironmentMiddleware
 
 logger = logging.getLogger(__name__)
 
@@ -25,31 +15,9 @@ async def main():
     logger.info("Starting core")
     config = load_config(".env")
 
-    if True:
-        bot = TelegramBot(config)
+    bot = TelegramBot(config)
 
-        bot.register_handler(CommandStartHandler())
-        bot.register_handler(EchoHandler())
-
-        await bot.try_start_pooling()
-    else:
-        storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage()
-        bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
-        dp = Dispatcher(bot, storage=storage)
-
-        bot['config'] = config
-
-        register_all_middlewares(dp, config)
-        register_all_filters(dp)
-        register_all_handlers(dp)
-
-        # start
-        try:
-            await dp.start_polling()
-        finally:
-            await dp.storage.close()
-            await dp.storage.wait_closed()
-            await bot.session.close()
+    await bot.try_start_pooling()
 
 
 if __name__ == '__main__':
